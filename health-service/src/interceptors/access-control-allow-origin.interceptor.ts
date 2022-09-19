219a76e57b3a3dae8ba4afd86896e08efa034ec2
MichaelDiers <md@sreid.de>
2022-09-19 20:20:18 +0200

    fix interceptor for response header settings

--- health-service/src/interceptors/access-control-allow-origin.interceptor.ts
+++ health-service/src/interceptors/access-control-allow-origin.interceptor.ts
@@ -3,8 +3,10 @@ import {
   NestInterceptor,
   ExecutionContext,
   CallHandler,
+  Inject,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import InjectionName from '../types/injection-name.enum';
 
 /**
  * Interceptor for response header handling.
@@ -12,11 +14,36 @@ import { Observable } from 'rxjs';
  */
 @Injectable()
 export class AccessControlAllowOriginInterceptor implements NestInterceptor {
+  /**
+   * Creates a new instance of AccessControlAllowOriginInterceptor.
+   * @param accessControlAllowOrigin The header is set only for this origin.
+   */
+  constructor(
+    @Inject(InjectionName.ACCESS_CONTROL_ALLOW_ORIGIN)
+    private readonly accessControlAllowOrigin: string,
+  ) {}
+
+  /**
+   * Intercept the response and set additional headers.
+   * @param context The current execution context.
+   * @param next The next handler.
+   * @returns An Observable<any>.
+   */
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const request = context.switchToHttp().getRequest();
-    if (request.get('Origin') === 'http://localhost:3000') {
+    const origin = request.get('Origin') as string;
+    if (
+      origin &&
+      this.accessControlAllowOrigin &&
+      origin
+        .toLowerCase()
+        .startsWith(this.accessControlAllowOrigin.toLocaleLowerCase())
+    ) {
       const response = context.switchToHttp().getResponse();
-      response.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+      response.set(
+        'Access-Control-Allow-Origin',
+        this.accessControlAllowOrigin,
+      );
       response.set('Vary', 'Origin');
     }
 
